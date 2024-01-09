f80c446887e7374584f838c92de7591ff36dedfe
MSRane <mohitsrane16@gmail.com>
2024-01-09 22:08:55 +0530

    dom test 2

--- 08_DOM/script.js
+++ 08_DOM/script.js
@@ -1,7 +1,18 @@
-var btn = document.querySelector('button')
-var p = document.querySelector('p')
+// var btn = document.querySelector('button')
+// var p = document.querySelector('p')
 
+// btn.addEventListener('click', function(){
+//     p.textContent = 'hey, buddy'
+// })
 
-btn.addEventListener('click', function(){
-    p.textContent = 'hey, buddy'
+var img1 = document.querySelector('#one')
+var img2 = document.querySelector('#two')
+
+document.querySelector('button')
+.addEventListener('click', function(){
+    var src1 = img1.src
+    var src2 = img2.src
+
+    img1.src = src2
+    img2.src = src1
 })
