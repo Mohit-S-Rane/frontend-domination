55d970528a6f9e627b14c390ef3b80779acfe934
Mohit Rane <mohitsrane16@gmail.com>
2024-01-10 10:42:52 +0530

    dom test 3

--- 08_DOM/script.js
+++ 08_DOM/script.js
@@ -5,14 +5,31 @@
 //     p.textContent = 'hey, buddy'
 // })
 
-var img1 = document.querySelector('#one')
-var img2 = document.querySelector('#two')
+// var img1 = document.querySelector('#one')
+// var img2 = document.querySelector('#two')
 
-document.querySelector('button')
-.addEventListener('click', function(){
-    var src1 = img1.src
-    var src2 = img2.src
+// document.querySelector('button')
+// .addEventListener('click', function(){
+//     var src1 = img1.src
+//     var src2 = img2.src
 
-    img1.src = src2
-    img2.src = src1
+//     img1.src = src2
+//     img2.src = src1
+// })
+
+
+var form = document.querySelector("form")
+var inps = document.querySelectorAll('input[type="text"]')
+var err = document.querySelector('h4')
+var lv_flag
+
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault()
+    for (var i = 0; i < inps.length; i++) {
+        if (inps[i].value.trim() === '') {
+            err.textContent = 'Error, some fields blank'
+            err.style.color = 'red'
+            break;
+        }
+    }
 })
