5af6c00b7d227397bb6a5a414ce96bf586fee4a1
MSRane <mohitsrane16@gmail.com>
2024-01-10 21:31:35 +0530

    gsap timeline

--- 09_GSAP/js/script.js
+++ 09_GSAP/js/script.js
@@ -6,10 +6,34 @@
 //     delay: 1
 // })
 
-gsap.from("#box",{
-    x:1000,
+// gsap.from("#box",{
+//     x:1000,
+//     duration: 2,
+//     rotate: 360,
+//     backgroundColor: "blue",
+//     delay: 1
+// })
+
+var tl = gsap.timeline()
+
+tl.to("#box1",{
+    x:1200,
+    rotation: 360,
+    scale: 0.5,
     duration: 2,
-    rotate: 360,
-    backgroundColor: "blue",
     delay: 1
+})
+
+tl.to("#box2",{
+    x:1200,
+    rotation: 360,
+    scale: 0.5,
+    duration: 2,
+})
+
+tl.to("#box3",{
+    x:1200,
+    rotation: 360,
+    scale: 0.5,
+    duration: 2,
 })
