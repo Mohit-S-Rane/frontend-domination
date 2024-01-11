036c407d7a56d86e06dadcdf43bfebd7981c043b
MSRane <mohitsrane16@gmail.com>
2024-01-11 21:44:33 +0530

    page1 done

--- 10_OBYS_PROJECT/script.js
+++ 10_OBYS_PROJECT/script.js
@@ -32,19 +32,28 @@ tl.to(".line h2",{
 tl.to("#loader", {
     opacity: 0,
     duration: 0.4,
-    delay: 4
+    delay: 0
 })
 
 tl.from("#page1",{
     delay: 0.2,
     y: 1600,
     opacity: 0,
-    duration: 0.5,
+    duration: 0.6,
     ease: Power4
 })
 
 tl.to("#loader",{
     display: "none"
+})
+
+tl.from("#nav",{
+    opacity: 0
+})
+
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+    y: 120,
+    stagger: 0.2
 })
 }
 function cursorAnimation(){
