b75ff33eabfc5b836937b271d4a27c48a9d6d5fb
Mohit Rane <mohitsrane16@gmail.com>
2024-01-13 13:57:47 +0530

    project done

--- 10_OBYS_PROJECT/script.js
+++ 10_OBYS_PROJECT/script.js
@@ -177,11 +177,52 @@ function flagAnimation() {
     })
 }
 
+function footerAnimation() {
+
+    var clutter = ""
+    var clutter2 = ""
+    document.querySelector("#footer h1").textContent.split("").forEach(function (elem) {
+        clutter += `<span>${elem}</span>`
+    })
+    document.querySelector("#footer h1").innerHTML = clutter
+    document.querySelector("#footer h2").textContent.split("").forEach(function (elem) {
+        clutter2 += `<span>${elem}</span>`
+    })
+    document.querySelector("#footer h2").innerHTML = clutter2
+
+
+    document.querySelector("#footer-text").addEventListener("mouseenter", function () {
+        gsap.to("#footer h1 span", {
+            opacity: 0,
+            stagger: 0.05
+        })
+        gsap.to("#footer h2 span", {
+            delay: 0.35,
+            opacity: 1,
+            stagger: 0.1
+        })
+    })
+    document.querySelector("#footer-text").addEventListener("mouseleave", function () {
+        gsap.to("#footer h1 span", {
+            opacity: 1,
+            stagger: 0.1,
+            delay: 0.35,
+
+        })
+        gsap.to("#footer h2 span", {
+            opacity: 0,
+            stagger: 0.05
+        })
+    })
+}
+
+
 loadingAnimation()
 cursorAnimation()
 locomotiveAnimation()
 sheryAnimation()
 flagAnimation()
+footerAnimation()
 
 
 
